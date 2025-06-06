e9edc7a46d654741c9dd46c5c453c62da3d4400b
Gunnar Grosch <gunnar@grosch.se>
2025-06-06 22:36:34 +0200

    Updated sample app dependencies

---
+++ tests/unit/test-handler.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import { lambdaHandler } from '../../src/index.mjs';
+import { expect, describe, it } from '@jest/globals';
+
+describe('Unit test for app handler', function () {
+    it('verifies successful response', async () => {
+        const event = {
+            httpMethod: 'GET'
+        };
+        const context = {};
+        
+        const result = await lambdaHandler(event, context);
+        
+        expect(result).toHaveProperty('statusCode');
+        expect(result.statusCode).toEqual(200);
+        expect(result).toHaveProperty('body');
+        
+        const response = JSON.parse(result.body);
+        
+        expect(response).toHaveProperty('message');
+        expect(response.message).toEqual('hello world');
+    });
+});
